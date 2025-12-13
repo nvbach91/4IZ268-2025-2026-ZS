19af3c0ca3e0c3c175b78766365a0763c5ca5c55
JakubMalickay <malickayjjj@gmail.com>
2025-12-13 19:06:33 +0100

    malj23-sp2

--- sp2/js/elements.js
+++
@@ -1,7 +0,0 @@
-// DOM element references
-const loginBtn = $('#loginBtn');
-const logoutBtn = $('#logoutBtn');
-const searchBtn = $('#searchBtn');
-const artistInput = $('#artistInput');
-const result = $('#result');
-const searchResult = $('#searchResult');
