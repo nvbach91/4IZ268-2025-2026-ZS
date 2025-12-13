21b58971f15f7a9fa879dfba2e0b45c60e7f3259
JakubMalickay <malickayjjj@gmail.com>
2025-12-13 19:04:13 +0100

    file structure repair

---
+++ www/malj23/sp2/js/elements.js
@@ -0,0 +1,7 @@
+// DOM element references
+const loginBtn = $('#loginBtn');
+const logoutBtn = $('#logoutBtn');
+const searchBtn = $('#searchBtn');
+const artistInput = $('#artistInput');
+const result = $('#result');
+const searchResult = $('#searchResult');
