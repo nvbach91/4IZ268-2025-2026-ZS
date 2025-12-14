bef3f908f6155dde8db335cce4a565a4440c4769
diavajs <dvajsova13@gmail.com>
2025-12-14 16:27:23 +0100

    vajd00-sp2
    Pilotná verzia 
    https://eso.vse.cz/~vajd00/sp2/

---
+++ www/vajd00/sp2/assets/js/main.js
@@ -0,0 +1,8 @@
+console.log("Budget Tracker loaded (pilot)");
+
+// TODO:
+// - load categories via fetch (categories.json)
+// - load transactions from localStorage
+// - render table + summary dynamically
+// - fetch exchange rate from Frankfurter API
+// - draw chart via Chart.js
